b2f70d5ab2c11602611f14ef075d98e7892ccd24
Cheng Li <cli12@mail.sfsu.edu>
2016-11-18 12:23:36 -0700

    fix the error in gatorRenter_autor.sql

--- application/controller/api/login.php
+++ application/controller/api/login.php
@@ -19,20 +19,21 @@ class Login extends Controller
             session_start();
         }
 
-//        header('Content-Type: application/json;charset=UTR-8');
+        header('Content-Type: application/json;charset=UTR-8');
         $email = $_GET["loginname"];
         $password = $_GET["password"];
         $user_info = $this->model->getUserInfo($email);
         if ($user_info == false) {
-            echo json_encode(array('valid_username' => false, 'valid_password' => false, 'authenic_user' => false));
+            echo json_encode(array('valid_username' => false, 'valid_password' => false, 'authentic_user' => false));
             return;
         }
+
 
         if ($user_info->password == $password) {
             $_SESSION['user_name'] = $email;
             $_SESSION['password'] = $password;
             $_SESSION['authentic_user'] = true;
-            echo json_encode(array('valid_username' => true, 'valid_password' => true, 'authenic_user' => true));
+            echo json_encode(array('valid_username' => true, 'valid_password' => true, 'authentic_user' => true));
 //            echo json_encode($_SESSION);
         } else {
             echo json_encode(array('valid_username' => true, 'valid_password' => false, 'authentic_user' => false));
